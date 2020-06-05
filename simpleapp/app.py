813e1432d92c0d5382842509293fe57bf53d4bbb
mauro.cossu@oracle.com <mauro.cossu@oracle.com>
2020-06-05 11:45:49 +0100

    added more gifs

--- simpleapp/app.py
+++ simpleapp/app.py
@@ -17,13 +17,13 @@ images = [
     "http://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-26390-1381844163-18.gif",
     "http://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-26388-1381844103-11.gif",
     "http://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-27162-1381845360-0.gif",
-    "http://www.dogshaming.co.uk/wp-content/uploads/2014/11/10976_10152751004061385_6101333622735638035_n.jpg",
-    "https://tenor.com/view/funny-dog-gif-7753357",
-    "https://media.tenor.com/images/a10e992704a5d395217800f925391da9/tenor.gif",
-    "https://tenor.com/view/turtle-animals-animated-save-the-turtle-dog-gif-15349938",
-    "https://post.barkbox.com/?attachment_id=1561",
     "https://s3.amazonaws.com/barkpost-assets/50+GIFs/3.gif",
-    "https://s3.amazonaws.com/barkpost-assets/50+GIFs/1.gif"
+    "https://s3.amazonaws.com/barkpost-assets/50+GIFs/1.gif",
+    "https://66.media.tumblr.com/133c423fd39ca03fda132071d82d8693/tumblr_n0j8isqnIq1si7ew2o1_400.gifv",
+    "https://66.media.tumblr.com/1641e08473c7df8a56d52de2e5c55219/tumblr_nbtr8twYCL1qbxi45o1_500.gifv",
+    "https://66.media.tumblr.com/19f67e90f04dbe57f52677076d742522/tumblr_n61npz36HJ1svxaato1_250.gifv",
+    "https://s3.amazonaws.com/barkpost-assets/50+GIFs/45.gif",
+    "https://s3.amazonaws.com/barkpost-assets/50+GIFs/38.gif"
 ]
 
 @app.route('/')
